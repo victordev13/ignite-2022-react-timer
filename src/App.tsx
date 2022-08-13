1478c4a6312a09af5a44a2a9fe1fddb3f11c9096
victordev13 <vtrcarvalho.13@gmail.com>
2022-08-13 19:35:23 -0300

    styled components default theme with typescript integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
-  return <div></div>;
+  return <ThemeProvider theme={defaultTheme}></ThemeProvider>;
 }
-
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,6 @@
+export const defaultTheme = {
+    colors: {
+        primary: 'blue',
+        secondary: 'brown'
+    }
+}
